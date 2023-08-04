5616171daccbfb07d037161b425a336fa7ff28b3
AlexRazor <alexrazor1337@gmail.com>
2023-08-04 13:45:50 +0300

    Get user post single, updates to some post logic

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+const path = require('path');
+
 const express = require('express');
 const mongoose = require('mongoose');
 
@@ -13,8 +15,20 @@ app.use((req, res, next) => {
     next();
 });
 
+app.use('/images', express.static(
+    path.join(__dirname, 'images')
+));
+
 app.use('/feed', feedRoutes);
 
+app.use((error, req, res, next) => {
+    console.log(error);
+    const status = error.statusCode || 500;
+    const { message } = error;
+    res.status(status).json({ message });
+});
+
 mongoose.connect('mongodb://localhost:27017/posts', { useNewUrlParser: true, useUnifiedTopology: true }).then(result => {
+    console.log('Connected to MongoDB');
     app.listen(3000);
 }).catch(err => console.log(err));
