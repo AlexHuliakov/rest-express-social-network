99d1868cdfb754135d0b5b8f299481a9999c4c92
AlexRazor <alexrazor1337@gmail.com>
2023-08-07 02:26:20 +0300

    Add tests

---
+++ test/feed-controller.js
@@ -0,0 +1,66 @@
+import { expect } from 'chai';
+import mongoose from 'mongoose';
+
+import User from '../models/user.js';
+import FeedController from '../controllers/feed.js';
+
+describe('Feed Controller', function() {
+  before(function(done) {
+    mongoose
+      .connect(
+        'mongodb+srv://maximilian:fmFLrH6d0DjMxWcg@cluster0-ntrwp.mongodb.net/test-messages?retryWrites=true'
+      )
+      .then(result => {
+        const user = new User({
+          email: 'test@test.com',
+          password: 'tester',
+          name: 'Test',
+          posts: [],
+          _id: '5c0f66b979af55031b34728a'
+        });
+        return user.save();
+      })
+      .then(() => {
+        done();
+      });
+  });
+
+  beforeEach(function() {});
+
+  afterEach(function() {});
+
+  it('should add a created post to the posts of the creator', function(done) {
+    const req = {
+      body: {
+        title: 'Test Post',
+        content: 'A Test Post'
+      },
+      file: {
+        path: 'abc'
+      },
+      userId: '5c0f66b979af55031b34728a'
+    };
+    const res = {
+      status: function() {
+        return this;
+      },
+      json: function() {}
+    };
+
+    FeedController.createPost(req, res, () => {}).then(savedUser => {
+      expect(savedUser).to.have.property('posts');
+      expect(savedUser.posts).to.have.length(1);
+      done();
+    });
+  });
+
+  after(function(done) {
+    User.deleteMany({})
+      .then(() => {
+        return mongoose.disconnect();
+      })
+      .then(() => {
+        done();
+      });
+  });
+});
